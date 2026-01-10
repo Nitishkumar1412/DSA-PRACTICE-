5f01f3cca6c1238f3028c479c740840996b70001
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-10 19:48:43 +0530

    GFG Arrays: Search insert position of K in a sorted array

--- LeetCode/Arrays/Shuffle_the_array.cpp
+++ LeetCode/Arrays/Shuffle_the_array.cpp
@@ -1,16 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
-        int i=0;;
+        int i = 0;
 
-        while(i<n){
+        while (i < n) {
             ans.push_back(nums[i]);
-            ans.push_back(nums[i+n]);
+            ans.push_back(nums[i + n]);
             i++;
-
         }
         return ans;
-        
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 5, 1, 3, 4, 7};
+    int n = 3;
+
+    vector<int> result = sol.shuffle(nums, n);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
