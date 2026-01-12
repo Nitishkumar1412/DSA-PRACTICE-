054eea0a31caf2a23f6cc2d991072e5af1eaabd4
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-12 21:32:40 +0530

    GFG Arrays: Majority Elements.

---
+++ GeeksForGeeks/Arrays/Majority_Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+
+        int candidate = arr[n / 2];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            }
+        }
+
+        if (count > n / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+};
