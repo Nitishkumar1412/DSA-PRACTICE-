790bab5395867c326ac506b42a1227f0b352dcaf
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-13 19:47:10 +0530

    GFG Mathematics:Check an Integer is power of 3 or not .

---
+++ GeeksForGeeks/Mathematics/Check_an_Integer_is_power_of_3_or_not.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    string isPowerof3(int N) {
+        if(N<=0) return "No";
+        if(N==1)    return "Yes";
+        if(N%3!=0)    return "No";
+        return isPowerof3(N/3);
+        
+    }
+};
