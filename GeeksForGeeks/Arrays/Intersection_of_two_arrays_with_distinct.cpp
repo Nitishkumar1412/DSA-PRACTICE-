89424d2b95a01761047070d24f5478efd3e54532
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-20 23:50:45 +0530

    GFG Arrays: Intersection of two arrays with distinct.

---
+++ GeeksForGeeks/Arrays/Intersection_of_two_arrays_with_distinct.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int intersectSize(vector<int> &a, vector<int> &b) {
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        int i = 0, j = 0;
+        int count = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] < b[j]) {
+                i++;
+            }
+            else if (a[i] > b[j]) {
+                j++;
+            }
+            else {
+                count++;  // common element found
+                i++;
+                j++;
+            }
+        }
+        return count;
+    }
+};
