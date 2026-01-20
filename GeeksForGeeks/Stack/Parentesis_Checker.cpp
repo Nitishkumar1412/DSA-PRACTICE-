98d5a2f064ece2a75f817f51b27e99285f2116c3
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-20 23:36:38 +0530

    GFG Stack: Parenthesis Checker.

---
+++ GeeksForGeeks/Stack/Parentesis_Checker.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            char ch = s[i];
+            
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+                
+            }
+            else{
+                if(!st.empty()){
+                    if(ch==')' && st.top()=='(' || ch=='}' && st.top()=='{' || ch==']' && st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
+            
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
