9bd363d280c3d73d6e41c1ce2ec2d358eda80d56
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-21 22:41:15 +0530

    GFG Arrays: Kadane's Algorithm.

---
+++ GeeksForGeeks/Arrays/Kadane's_Algorithm.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        for(int i=0; i<arr.size(); i++){
+            currSum += arr[i];
+            maxSum = max(currSum, maxSum);
+            if(currSum<0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
