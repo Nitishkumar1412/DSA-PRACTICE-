f90d3e3d08729813f4f4675814dd72d4fb3ee060
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-22 14:16:48 +0530

    GFG Arrays: GCD of Array.

---
+++ GeeksForGeeks/Arrays/GCD_of_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    int gcd(int n, vector<int>& arr) {
+        int ans = arr[0];
+        for (int i = 1; i < n; i++) {
+            ans = gcd(ans, arr[i]);
+            if (ans == 1) return 1; // early stopping
+        }
+        return ans;
+    }
+};
