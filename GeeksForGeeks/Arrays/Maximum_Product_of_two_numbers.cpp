fd29f4c4d56ca25755450de3094a4e88e293beb7
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-23 15:49:22 +0530

    GFG Arrays: Maximum Product of two numbers.

---
+++ GeeksForGeeks/Arrays/Maximum_Product_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+
+    int maxProduct(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        
+        return arr[n-1]*arr[n-2];
+        
+    }
+};
