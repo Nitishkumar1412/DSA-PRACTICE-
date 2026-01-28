79b928f234485bbaf638e4f44bf5b7dea01a4b4b
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-01-28 23:40:43 +0530

    GFG Arrays: Value equal to index equal.

---
+++ GeeksForGeeks/Arrays/Value_equal_to_index_value.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> valueEqualToIndex(vector<int>& arr) {
+        vector<int> ans;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == i + 1) {
+                ans.push_back(i + 1); 
+            }
+        }
+
+        return ans;
+    }
+};
