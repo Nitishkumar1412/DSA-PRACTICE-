849088b8003fe2eab844a7c7a9f68cf8d833131b
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-02-11 00:18:51 +0530

    GFG Arrays: Search in rotated sorted array.

---
+++ GeeksForGeeks/Arrays/Search_in_rotated_sorted_array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        int n = arr.size();
+        int start = 0, end = n-1;;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(arr[mid]==key){
+                return mid;
+            }
+            if(arr[start]<=arr[mid]){ //left Sorted
+                if(arr[start]<= key && key <= arr[mid] ){
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else{ // Right Sorted
+                if(arr[mid]<= key && key <= arr[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
