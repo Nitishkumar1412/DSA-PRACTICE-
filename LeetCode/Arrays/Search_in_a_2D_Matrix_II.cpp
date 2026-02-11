63621e96095a82aab41eebf979a79d402270d72e
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-02-11 22:52:05 +0530

    Leetcode Arrays: Search a 2D Matrix II.

---
+++ LeetCode/Arrays/Search_in_a_2D_Matrix_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(); // No. of Rows
+        int n = matrix[0].size(); // No. of Columns;
+
+        // Taking Top Right Cell.
+        int i = 0;
+        int j = n-1;  
+
+        while(i<m && j>=0){ // Downward Traverse
+            if(matrix[i][j]==target)    return true;
+            else if(matrix[i][j]>target)    j--; // Column - 1
+            else if(matrix[i][j]<target)    i++; // Row + 1
+
+        }
+        return false;
+    }
+};
