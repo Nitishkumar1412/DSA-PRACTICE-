de70073cc33aeb36eddd9f0255be4ae21479fe40
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-02-16 22:39:15 +0530

    GFG Strings: Remove Consecutive Characters.

---
+++ GeeksForGeeks/Strings/Remove_Consecutive_Characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string removeConsecutiveCharacter(string& s) {
+        int n = s.size();
+        string result = "";
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                continue;
+            }
+            else
+            {
+                result += s[i];
+            }
+        }
+        return result+ s[n-1];
+    }
+};
