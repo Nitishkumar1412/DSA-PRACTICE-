372e72dcc21556154acebc0bd69077b1aa017659
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-03-07 22:12:08 +0530

    GFG Arrays: Union of 2 Sorted Arrays.

---
+++ GeeksForGeeks/Arrays/Union_of_2_sorted_array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        vector<int> res;
+        int n1 = a.size();
+        int n2 = b.size();
+        int i = 0, j = 0;
+
+       
+        while (i < n1 && j < n2) {
+            if (a[i] <= b[j]) {
+                if (res.empty() || res.back() != a[i]) {
+                    res.push_back(a[i]);
+                }
+                i++;
+            } else { 
+                if (res.empty() || res.back() != b[j]) {
+                    res.push_back(b[j]);
+                }
+                j++;
+            }
+        }
+ 
+        while (i < n1) {
+            if (res.empty() || res.back() != a[i]) {
+                res.push_back(a[i]);
+            }
+            i++;
+        }
+        while (j < n2) {
+            if (res.empty() || res.back() != b[j]) {
+                res.push_back(b[j]);
+            }
+            j++;
+        }
+
+        return res;
+    }
+    };
