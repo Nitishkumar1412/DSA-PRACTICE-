273a098e788e8b174bc96bc029579e2473028f3f
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-03-11 17:23:01 +0530

    GFG  Arrays: Indexes of subarray sum.

---
+++ GeeksForGeeks/Arrays/Indexes_of_subarry_sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        
+        int start = 0;
+        long long sum = 0;
+        
+        for(int end = 0; end < arr.size(); end++) {
+            
+            sum += arr[end];
+            
+            while(sum > target && start <= end) {
+                sum -= arr[start];
+                start++;
+            }
+            
+            if(sum == target) {
+                return {start + 1, end + 1}; // 1-based index
+            }
+        }
+        
+        return {-1};
+    }
+};
