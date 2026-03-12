beb1555664585b8788f23f1947a371558f55345b
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-03-12 21:03:55 +0530

    GFG Strings: Non Repeating Character.

---
+++ GeeksForGeeks/Strings/Non_Repeating_Character.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char nonRepeatingChar(string &s) {
+        int freq[26] = {0};
+
+        // Count frequency
+        for(char c : s) {
+            freq[c - 'a']++;
+        }
+
+        // Find first non-repeating character
+        for(char c : s) {
+            if(freq[c - 'a'] == 1)
+                return c;
+        }
+
+        return '$';
+    }
+};
