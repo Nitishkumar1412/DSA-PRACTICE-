3fcb3148eadff4d53350176b3a91678dd0ba37b7
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-03-15 19:59:08 +0530

    Leetcode Arrays: Rotate Array.

---
+++ LeetCode/Arrays/Rotate_Array.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        std::rotate(nums.begin(), nums.begin() + (n - k), nums.end());
+    }
+};
