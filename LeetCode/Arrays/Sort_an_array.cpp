f54ce396dff8b77b9eb666382d05fa2f28e8fda9
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-03-27 01:16:01 +0530

    Leetcode Arrays: Sort an array.

---
+++ LeetCode/Arrays/Sort_an_array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int si, int mid, int ei) {
+        vector<int> temp;
+        int i = si;
+        int j = mid + 1;
+
+        while (i <= mid && j <= ei) {
+            if (nums[i] <= nums[j]) {
+                temp.push_back(nums[i++]);
+            } else {
+                temp.push_back(nums[j++]);
+            }
+        }
+
+        while (i <= mid) {
+            temp.push_back(nums[i++]);
+        }
+
+        while (j <= ei) {
+            temp.push_back(nums[j++]);
+        }
+
+        for (int idx = si, x = 0; idx <= ei; idx++, x++) {
+            nums[idx] = temp[x];
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        mergeSort(nums, si, mid);
+        mergeSort(nums, mid + 1, ei);
+
+        merge(nums, si, mid, ei);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        mergeSort(nums, 0, n - 1);
+        return nums;
+    }
+};
