c9ec93d7c435808211d2388bd4acfdec4f425f11
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-05-19 21:55:35 +0530

    GFG Arrays: Minimum Multiplications to reach End .

---
+++ GeeksForGeeks/Arrays/Minimum_Multiplications_to_reach_End.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minSteps(vector<int>& arr, int start, int end) {
+        
+        if(start == end) return 0;
+        
+        vector<int> dist(1000, 1e9);
+        queue<pair<int,int>> q;
+        
+        q.push({start, 0});
+        dist[start] = 0;
+        
+        while(!q.empty()) {
+            
+            auto it = q.front();
+            q.pop();
+            
+            int node = it.first;
+            int steps = it.second;
+            
+            for(int x : arr) {
+                
+                int num = (node * x) % 1000;
+                
+                if(steps + 1 < dist[num]) {
+                    
+                    dist[num] = steps + 1;
+                    
+                    if(num == end)
+                        return steps + 1;
+                    
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
