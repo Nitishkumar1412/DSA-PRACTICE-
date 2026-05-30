67daabf9b3b69089ed3f8718310b5833f0993d32
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-05-30 21:24:36 +0530

    GFG Arrays: Replace with XOR of Adjacent.

---
+++ GeeksForGeeks/Arrays/Replace_with_XOR_of_Adjacent.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void replaceElements(vector<int>& arr) {
+        int n = arr.size();
+        
+        if (n == 1) return;
+
+        vector<int> temp = arr;
+
+        arr[0] = temp[0] ^ temp[1];
+
+        for (int i = 1; i < n - 1; i++) {
+            arr[i] = temp[i - 1] ^ temp[i + 1];
+        }
+
+        arr[n - 1] = temp[n - 2] ^ temp[n - 1];
+    }
+};
