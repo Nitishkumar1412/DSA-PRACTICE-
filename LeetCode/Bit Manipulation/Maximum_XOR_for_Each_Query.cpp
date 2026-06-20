4cc51759ae484357cee311500123b94e0961dc3f
Nitishkumar1412 <mr.nitishbakhetia@gmail.com>
2026-06-20 21:29:01 +0530

    Leetcode Bit Manipulation: Maximum XOR for Each Query.

---
+++ LeetCode/Bit Manipulation/Maximum_XOR_for_Each_Query.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:	
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+        vector<int> result(n);
+        
+        int XOR = 0;
+        for (int i = 0; i < n; i++) {
+            XOR ^= nums[i];
+        }
+
+        int mask = (1 << maximumBit) - 1;
+
+        for (int i = 0; i < n; i++) {
+            result[i] = mask ^ XOR;
+            XOR ^= nums[n - i - 1];
+        }
+
+        return result;
+    }
+};
